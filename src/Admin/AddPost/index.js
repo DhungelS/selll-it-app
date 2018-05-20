ad6525662af9cd65210a9a50af5928a53128e42a
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-05-20 10:52:32 -0400

    navigation setup

---
+++ src/Admin/AddPost/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class AddPost extends Component {
+  render() {
+    return <Text>Add post</Text>;
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default AddPost;
---
+++ src/Login/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Login extends Component {
+  render() {
+    return <Text>Login</Text>;
+  }
+}
+
+const styles = StyleSheet.create({});
+
+export default Login;
